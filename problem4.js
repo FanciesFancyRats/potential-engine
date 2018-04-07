48af0b69ea891ef55269e90b54037ec2698fc5d1
FanciesFancyRats <magriffi1986@gmail.com>
2018-04-07 06:43:06 -0500

    That was a lot easier than I thought

---
+++ problem4.js
@@ -0,0 +1,13 @@
+unsolved = false;
+var x = 999;
+var y = 999;
+
+while(unsolved){
+	
+}
+function isPal(number){
+	var check = String(number);
+	check = check.split('');	
+	console.log(check);
+}
+isPal(x);
