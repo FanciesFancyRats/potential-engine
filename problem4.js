b90ba2e1135239e83c4afb382c8c3afca74948ba
FanciesFancyRats <magriffi1986@gmail.com>
2018-04-07 12:55:26 -0500

    isPal returns undifined if recurse

--- problem4.js
+++ problem4.js
@@ -1,13 +1,45 @@
 unsolved = false;
-var x = 999;
+var x = 777;
 var y = 999;
 
 while(unsolved){
 	
 }
 function isPal(number){
+	console.log(number);
 	var check = String(number);
 	check = check.split('');	
-	console.log(check);
+	console.log('check.length: ', check.length);
+	if(check.length == 1){
+		return true ;
+	}
+	if(check.length == 2){
+		console.log('check.length == 2');	
+		console.log(check[0], check[1]);
+		console.log(check[0] == check[1]);
+		if(check[0] == check[1]){
+			return true;	
+		}	
+		else{
+			return false;	
+		}
+	}
+	else{
+		if(check[0] == check[check.length - 1]){
+			check.splice(0, 1);
+			check.splice((check.length-1), 1);
+			number = check.join('');
+			isPal(number);
+		}	
+		else{
+			return false ;	
+		}
+	}
 }
-isPal(x);
+if(isPal(x)){
+	console.log('Is pal');
+}
+else{
+	console.log('is not pal');
+}
+
