d97b89455db884c23f556b20b56c91ec281ccfd5
FanciesFancyRats <magriffi1986@gmail.com>
2018-05-20 12:35:21 -0500

    some more helper functions

--- p3.scm
+++ p3.scm
@@ -16,13 +16,35 @@
   (lambda (n adder limit numbers)
     (cond
       ((> n limit) numbers)
-      ((memeber n numbers) (add-until (+ n adder) adder limit numbers))
+      ((member n numbers) (add-until (+ n adder) adder limit numbers))
       (else (add-until (+ n adder) adder limit (cons n numbers))))))
 
-(define sieve
+(define sieve-iter
   (lambda (n limit notPrimes primes)
     (cond
 	((> n limit) primes)
-	((and (= (modulo n 2) 0) (> n 2)) (sieve (+ n 1) limit notPrimes primes))
-	((member n notPrimes) (sieve (+ n 1) limit notPrimes primes))
-	(else (sieve (+ n 1) limit (add-until n n limit notPrimes) (cons n primes))))))
+	;((and (= (modulo n 2) 0) (> n 2)) (sieve (+ n 1) limit notPrimes primes))
+	((member n notPrimes) (sieve-iter (+ n 1) limit notPrimes primes))
+	(else (sieve-iter (+ n 1) limit (add-until n n limit notPrimes) (cons n primes))))))
+
+(define sieve
+  (lambda (limit)
+    (sieve-iter 2 limit () ())))
+
+;(define is-multiple
+;  (lambda (n listn)
+(define list8 '(2 2 2))
+(define listnumbers '(1 2 3 4 5 6 7 8 9))
+
+
+(define multiply-list
+  (lambda (n listn product)
+    (cond
+      ((null? listn) (* n product))
+      (else (multiply-list (car listn) (cdr listn) (* n product))))))
+
+(define iter-list
+  (lambda (n limit alist)
+    (cond 
+      ((> n limit) (car alist))
+      (else (iter-list (+ n 1) limit (cdr alist))))))
