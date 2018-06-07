3ba31b74ff5ed11b8ad31d171b9dcc1a825316fd
FanciesFancyRats <magriffi1986@gmail.com>
2018-06-07 12:12:24 -0500

    Sieve still sucks

--- p3-2.scm
+++ p3-2.scm
@@ -1,14 +1,18 @@
 (define add-multiple-to-list-iter
   (lambda (multiple lst n limit)
     (cond
-      ((= n limit) lst)
+      ((> n limit) lst)
       ((member? n lst) (add-multiple-to-list-iter multiple lst (+ n multiple) limit))
       (else (add-multiple-to-list-iter multiple (cons n lst) (+ n multiple) limit)))))
+(define (sieve n)
+  (sieve-inter n (add-multiple-to-list-iter 2 () 4 n) '(2 1) 3))
 
-(define sieve
-  (lambda (n)
-    (sieve-iter n 3 (add-multiple-to-list-iter 2 () 2 n))))
+(define sieve-inter 
+  (lambda (limit notPrimes primes n)
+    (cond
+      ((> n limit) primes)
+     
+      ((member? n  notPrimes) (sieve-inter limit notPrimes primes (+ n 2)))
+      ;(else (sieve-inter limit notPrimes primes (+ n 2))))))
+      (else (sieve-inter limit (add-multiple-to-list-iter n notPrimes (+ n n) limit) (cons n primes) (+ n 2))))))
 
-(define sieve-iter
-  (lambda (limit n notPrimes)
-    notPrimes))
