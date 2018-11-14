ae698ef4f1a184c6d0dbe6e1c29dc25eb1419098
FanciesFancyRats <magriffi1986@gmail.com>
2018-11-14 06:37:26 -0600

    on section 5...

---
+++ tj502.java
@@ -0,0 +1,30 @@
+public class tj502 {
+	public static void zoop(String fred, int bob){
+		System.out.println(fred);
+	if ( bob == 5) {
+			ping("not ");
+		}	
+		else{
+			System.out.println("!");
+		}
+	}
+	public static void main(String[] args){
+		int bizz = 5;
+		int buzz = 2;
+		zoop("just for", bizz);
+		clink(2 * buzz);	
+		System.out.println("Expected output: ");
+		System.out.println("just for ");
+	       	System.out.println("any not more ");
+		System.out.println("It's breakfast ");
+		System.out.println("!");
+	}
+	public static void clink(int fork){
+		System.out.print("It's ");
+		zoop("breakfast ", fork);
+
+	}
+	public static void ping(String strangStrung){
+		System.out.println("any " + strangStrung + "more ");
+	}
+}
---
+++ tj504.java
@@ -0,0 +1,10 @@
+
+public class tj504 {
+	public static void main(String[] args){
+		System.out.println(Math.pow(2, 2));
+		
+	}
+	public static void checkFermat(int a, int b, int c, int n){
+		System.out.println("Can you hear me?");	
+	}
+}
