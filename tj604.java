19a0e2d162edef437a0a7ef9c0398c031f4a77d9
FanciesFancyRats <magriffi1986@gmail.com>
2018-11-20 20:24:00 -0600

    604 progress

--- tj604.java
+++ tj604.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class tj604{
 	public static void main(String[] args){
-		System.	
+		double pi, n;
+		Scanner in = new Scanner(System.in);
+		pi = Math.sin(3.14159);
+		System.out.println(multadd(1.0, 2.0, 3.0));
+		System.out.println(multadd((Math.cos(3.14159/4)), (1/2), (Math.sin(3.14159/4))));
+		n = in.nextInt();	
+
+
+	}
+	public static double multadd(double a, double b, double c){
+		return(a * b + c);			
+	}
+	public static double expSum(double n){
+		return(multadd(n, Math.E, Math.PI));	
 	}
 }
